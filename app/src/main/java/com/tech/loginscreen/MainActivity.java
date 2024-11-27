e79f8f6d529b5ff8f243942a6b20088ac9199198
Maryam mohamed <mm3279@fayoum.edu.eg>
2024-11-27 16:53:40 +0200

    Initial commit

---
+++ app/src/main/java/com/tech/loginscreen/MainActivity.java
@@ -0,0 +1,37 @@
+package com.tech.loginscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button mButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usernameEditText = findViewById(R.id.eTUserName);
+        passwordEditText = findViewById(R.id.eTPassword);
+        mButton = findViewById(R.id.btLogin);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+    
+}
